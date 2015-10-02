c6e8972f4497ed7c26206c14b89d825ae2273730
emtudo <emtudo@gmail.com>
2015-10-02 07:18:09 -0300

    User or email auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name', 20);
             $table->string('email')->unique();
             $table->string('user')->unique();
             $table->string('password', 60);
