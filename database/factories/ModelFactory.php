ca7c2d1c1551b2a08bd03d79cdcae7899748f80e
Vinicius Reis <luiz.vinicius73@gmail.com>
2015-10-29 11:59:48 -0200

    fix password crypt

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,7 +12,7 @@ $factory->define(Artesaos\Domain\Users\User::class, function (Faker\Generator $f
         'name' => $faker->name,
         'username' => $username,
         'email' => strtolower($faker->email),
-        'password' => bcrypt(str_random(10)),
+        'password' => str_random(10),
         'remember_token' => str_random(10),
     ];
 });
